b7045a111b34c7b9382b14648977c99cc860c340
penelope15max <135342392+penelope15max@users.noreply.github.com>
2024-02-29 08:00:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+main.js
+const botoes = document.querySelectorAll(<<.botao>>);
+console.log(botoes)
+for (let i=0; i < botoes.length; i++){console.log(i);}
+for (let i=0;i <botoes.length;i++){
+    botoes[i].onclick = Function() {
+    botoes[i].classList.add(<<ativo>>);}}
